36e3c4c12d8ed42be7ec598d1215412383ef1afd
gloriakang <gkang@vt.edu>
2018-05-11 17:42:24 -0400

    update

---
+++ input/input_base.R
@@ -0,0 +1,10 @@
+# inputs = base case
+
+## base vaccine compliance = 0%
+vax_comp_b <- 0
+
+## total = 1248308.16 (36.64%)
+base_04 <- 74205.56 #33.19%
+base_519 <- 419788.08 #65.63%
+base_2064 <- 685774.72 #30.68%
+base_65 <- 68539.8 #22.21%
