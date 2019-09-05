512f83896724ed81c940f1d8d6f61284e00f6465
gloriakang <gkang@vt.edu>
2019-09-05 14:32:31 -0400

    confidence int

--- input/var.R
+++ input/var.R
@@ -1,6 +1,6 @@
 # global variables
+rm(list = ls(all.names = TRUE))
 
-library(stats)
 library(mc2d)
 
 # population sizes
@@ -34,94 +34,134 @@ rc_65_low <- c(33011, 12689, 922, 4)
 
 
 ##
-
 stat <- function(params) {
   set.seed(1000)
   q = quantile(params, c(0.025, 0.975))
   m = mean(params)
-  CI = c(m, q)
+  CI = c(q, m)
   return(CI)
 }
 
-x <- stat(rtriang(10000, min = 0.00036, mode = 0.001, max = 0.01821))
-x[1]
-x[2:3]
 
-
-p_04_high_death_CI <- stat(rtriang(10000, min = 0.00036, mode = 0.001, max = 0.01821))
-p_04_high_death_CI[2:3] # CI
-(p_04_high_death <- p_04_high_death_CI[1]) # mean
-
+### 0-4 high
 p_04_high_death_CI <- stat(rtriang(10000, min = 0.00036, mode = 0.001, max = 0.01821))
 p_04_high_hosp_CI <- stat(runif(10000, min = 0.006, max = 0.02143))
 p_04_high_out_CI <- stat(runif(10000, min = 0.82571, max = 0.95952))
-
-
-
-
-######
-
-# 0-4 high
-p_04_high_death <- mean(rtriang(10000, min = 0.00036, mode = 0.001, max = 0.01821))
-p_04_high_hosp <- mean(runif(10000, min = 0.006, max = 0.02143))
-p_04_high_out <- mean(runif(10000, min = 0.82571, max = 0.95952))
+#p_04_high_rest_CI1 <- 1 - (p_04_high_death_CI[1] + p_04_high_hosp_CI[1] + p_04_high_out_CI[1])
+#p_04_high_rest_CI2 <- 1 - (p_04_high_death_CI[2] + p_04_high_hosp_CI[2] + p_04_high_out_CI[2])
+#p_04_high_rest_CI <- c(p_04_high_rest_CI1, p_04_high_rest_CI2)
+# means
+p_04_high_death <- p_04_high_death_CI[3]
+p_04_high_hosp <- p_04_high_hosp_CI[3]
+p_04_high_out <- p_04_high_out_CI[3]
 p_04_high_rest <- 1-(p_04_high_death + p_04_high_hosp + p_04_high_out)
+#c(p_04_high_death, p_04_high_hosp, p_04_high_out, p_04_high_rest)
 
 
-# 0-4 low
-p_04_low_death <- mean(rtriang(10000, min = 0.00004, mode = 0.07/1000, max = 0.0003))
-p_04_low_hosp <- mean(runif(10000, min = 0.00057, max = 0.0069))
-p_04_low_out <- mean(runif(10000, min = 0.47143, max = 0.54762))
+### 0-4 low
+p_04_low_death_CI <- stat(rtriang(10000, min = 0.00004, mode = 0.07/1000, max = 0.0003))
+p_04_low_hosp_CI <- stat(runif(10000, min = 0.00057, max = 0.0069))
+p_04_low_out_CI <- stat(runif(10000, min = 0.47143, max = 0.54762))
+#p_04_low_rest_CI
+# means
+p_04_low_death <- p_04_low_death_CI[3]
+p_04_low_hosp <- p_04_low_hosp_CI[3]
+p_04_low_out <- p_04_low_out_CI[3]
 p_04_low_rest <- 1-(p_04_low_death + p_04_low_hosp + p_04_low_out)
-#(p_04_low_death, p_04_low_hosp, p_04_low_out, p_04_low_rest)
+#c(p_04_low_death, p_04_low_hosp, p_04_low_out, p_04_low_rest)
 
 
-# 5-19 high
-p_519_high_death <- mean(rtriang(10000, min = 0.00036, mode = 0.001, max = 0.01821))
-p_519_high_hosp <- mean(runif(10000, min = 0.006, max = 0.02143))
-p_519_high_out <- mean(runif(10000, min = 0.082571, max = 0.95952))
+### 5-19 high
+p_519_high_death_CI <- stat(rtriang(10000, min = 0.00036, mode = 0.001, max = 0.01821))
+p_519_high_hosp_CI <- stat(runif(10000, min = 0.006, max = 0.02143))
+p_519_high_out_CI <- stat(runif(10000, min = 0.082571, max = 0.95952))
+#p_519_high_rest_CI
+# means
+p_519_high_death <- p_519_high_death_CI[3]
+p_519_high_hosp <- p_519_high_hosp_CI[3]
+p_519_high_out <- p_519_high_out_CI[3]
 p_519_high_rest <- 1-(p_519_high_death + p_519_high_hosp + p_519_high_out)
-#(p_519_high_death, p_519_high_hosp, p_519_high_out, p_519_high_rest)
+#c(p_519_high_death, p_519_high_hosp, p_519_high_out, p_519_high_rest)
 
 
-# 5-19 low
-p_519_low_death <- mean(rtriang(10000, min = 0.00004, mode = 0.07/1000, max = 0.0003))
-p_519_low_hosp <- mean(runif(10000, min = 0.00057, max = 0.0069))
-p_519_low_out <- mean(runif(10000, min = 0.47143, max = 0.54762))
+### 5-19 low
+p_519_low_death_CI <- stat(rtriang(10000, min = 0.00004, mode = 0.07/1000, max = 0.0003))
+p_519_low_hosp_CI <- stat(runif(10000, min = 0.00057, max = 0.0069))
+p_519_low_out_CI <- stat(runif(10000, min = 0.47143, max = 0.54762))
+#p_519_low_rest_CI
+# means
+p_519_low_death <- p_519_low_death_CI[3]
+p_519_low_hosp <- p_519_low_hosp_CI[3]
+p_519_low_out <- p_519_low_out_CI[3]
 p_519_low_rest <- 1-(p_519_low_death + p_519_low_hosp + p_519_low_out)
-#(p_519_low_death, p_519_low_hosp, p_519_low_out, p_519_low_rest)
+#c(p_519_low_death, p_519_low_hosp, p_519_low_out, p_519_low_rest)
 
 
-# 20-64 high
-p_2064_high_death <- mean(runif(10000, min = 0.00083, max = 0.02487))
-p_2064_high_hosp <- mean(runif(10000, min = 0.00692, max = 0.02235))
-p_2064_high_out <- mean(runif(10000, min = 0.58333, max = 0.64786))
+### 20-64 high
+p_2064_high_death_CI <- stat(runif(10000, min = 0.00083, max = 0.02487))
+p_2064_high_hosp_CI <- stat(runif(10000, min = 0.00692, max = 0.02235))
+p_2064_high_out_CI <- stat(runif(10000, min = 0.58333, max = 0.64786))
+#p_2064_high_rest_CI
+# means
+p_2064_high_death <- p_2064_high_death_CI[3]
+p_2064_high_hosp <- p_2064_high_hosp_CI[3]
+p_2064_high_out <- p_2064_high_out_CI[3]
 p_2064_high_rest <- 1-(p_2064_high_death + p_2064_high_hosp + p_2064_high_out)
-#(p_2064_high_death, p_2064_high_hosp, p_2064_high_out, p_2064_high_rest)
+#c(p_2064_high_death, p_2064_high_hosp, p_2064_high_out, p_2064_high_rest)
 
 
-# 20-64 low
-p_2064_low_death <- mean(rtriang(10000, min = 0.00021, mode = 0.31/1000, max = 0.00039))
-p_2064_low_hosp <- mean(runif(10000, min = 0.0015, max = 0.01196))
-p_2064_low_out <- mean(runif(10000, min = 0.33333, max = 0.36957))
+### 20-64 low
+p_2064_low_death_CI <- stat(rtriang(10000, min = 0.00021, mode = 0.31/1000, max = 0.00039))
+p_2064_low_hosp_CI <- stat(runif(10000, min = 0.0015, max = 0.01196))
+p_2064_low_out_CI <- stat(runif(10000, min = 0.33333, max = 0.36957))
+#p_2064_low_rest_CI
+# means
+p_2064_low_death <- p_2064_low_death_CI[3]
+p_2064_low_hosp <- p_2064_low_hosp_CI[3]
+p_2064_low_out <- p_2064_low_out_CI[3]
 p_2064_low_rest <- 1-(p_2064_low_death + p_2064_low_hosp + p_2064_low_out)
-#(p_2064_low_death, p_2064_low_hosp, p_2064_low_out, p_2064_low_rest)
+#c(p_2064_low_death, p_2064_low_hosp, p_2064_low_out, p_2064_low_rest)
 
 
-# 65+ high
-p_65_high_death <- mean(runif(10000, min = 0.023, max = 0.02963))
-p_65_high_hosp <- mean(runif(10000, min = 0.03333, max = 0.06842))
-p_65_high_out <- mean(runif(10000, min = 0.65833, max = 0.68421))
+### 65+ high
+p_65_high_death_CI <- stat(runif(10000, min = 0.023, max = 0.02963))
+p_65_high_hosp_CI <- stat(runif(10000, min = 0.03333, max = 0.06842))
+p_65_high_out_CI <- stat(runif(10000, min = 0.65833, max = 0.68421))
+#p_65_high_rest_CI
+# means
+p_65_high_death <- p_65_high_death_CI[3]
+p_65_high_hosp <- p_65_high_hosp_CI[3]
+p_65_high_out <- p_65_high_out_CI[3]
 p_65_high_rest <- 1-(p_65_high_death + p_65_high_hosp + p_65_high_out)
-#(p_65_high_death, p_65_high_hosp, p_65_high_out, p_65_high_rest)
+#c(p_65_high_death, p_65_high_hosp, p_65_high_out, p_65_high_rest)
 
 
-# 65+ low
-p_65_low_death <- mean(rtriang(10000, min = 0.00233, mode = 3.51/1000, max = 4.52/1000))
-p_65_low_hosp <- mean(runif(10000, min = 0.0125, max = 0.01579))
-p_65_low_out <- mean(runif(10000, min = 0.375, max = 0.38947))
+### 65+ low
+p_65_low_death_CI <- stat(rtriang(10000, min = 0.00233, mode = 3.51/1000, max = 4.52/1000))
+p_65_low_hosp_CI <- stat(runif(10000, min = 0.0125, max = 0.01579))
+p_65_low_out_CI <- stat(runif(10000, min = 0.375, max = 0.38947))
+#p_65_low_rest_CI
+# means
+p_65_low_death <- p_65_low_death_CI[3]
+p_65_low_hosp <- p_65_low_hosp_CI[3]
+p_65_low_out <- p_65_low_out_CI[3]
 p_65_low_rest <- 1-(p_65_low_death + p_65_low_hosp + p_65_low_out)
-#(p_65_low_death, p_65_low_hosp, p_65_low_out, p_65_low_rest)
+#c(p_65_low_death, p_65_low_hosp, p_65_low_out, p_65_low_rest)
+
+### all
+
+c(p_04_high_death_CI, p_04_high_hosp_CI, p_04_high_out_CI)
+c(p_04_low_death_CI, p_04_low_hosp_CI, p_04_low_out_CI)
+c(p_519_high_death_CI, p_519_high_hosp_CI, p_519_high_out_CI)
+c(p_519_low_death_CI, p_519_low_hosp_CI, p_519_low_out_CI)
+c(p_2064_high_death_CI, p_2064_high_hosp_CI, p_2064_high_out_CI)
+c(p_2064_low_death_CI, p_2064_low_hosp_CI, p_2064_low_out_CI)
+c(p_65_high_death_CI, p_65_high_hosp_CI, p_65_high_out_CI)
+c(p_65_low_death_CI, p_65_low_hosp_CI, p_65_low_out_CI)
+
+
+## save RData
+#save.image(file = "output/vars.RData")
 
 
 
