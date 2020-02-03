22d74f170eb9f3996313840d3e476e70582f1024
gloriakang <gkang@vt.edu>
2020-02-02 23:11:36 -0500

    update again dammit

--- code/mod-figs.R
+++ code/mod-figs.R
@@ -1,12 +1,11 @@
-### new ggplot codes  
-rm(list=ls())
+
+rm(list = ls())
 
 datfull <- read.csv("df/icer-all.csv")
 
-### this is to order the facet columns the same way as the original figure
 datfull$risk_f = factor(datfull$risk, levels = c('High', 'Non-high', 'All'))
 
-### this is to order the legend 
+# order the legend 
 datfull$age <- factor(datfull$age, levels = c("0-4 yrs",
                                               "5-19 yrs",
                                               "20-64 yrs",
@@ -16,7 +15,7 @@ datfull$age <- factor(datfull$age, levels = c("0-4 yrs",
 f4 <- ggplot(subset(datfull, scenario %in% "vaxbase"),
              aes(y = cost.diff.per100k,
                  x = dalys.averted.per100k,
-                 group = age, colour = age)) +
+                 group = age, color = age)) +
   geom_point() + expand_limits(y = 0) +
   labs(y = "Cost saved per 100,000 population\n",
        x = "\nDALYs averted per 100,000 population",
@@ -29,14 +28,15 @@ f4 <- ggplot(subset(datfull, scenario %in% "vaxbase"),
   #       panel.grid.minor = element_line(colour = "grey86",size=0.5))+
   facet_grid(v.eff~risk_f, scales = "free")
 
-ggsave('figurex.png', height = 5, width = 7, dpi = 1200)
+#ggsave('figurex.png', height = 5, width = 7, dpi = 1200)
 
 f5 <- ggplot(subset(datfull, scenario %in% "vax70" & reference %in% "vaxbase"),
              aes(y = cost.diff.per100k,
                  x = dalys.averted.per100k,
                  group = age, colour = age)) + geom_point()+
-  expand_limits(y=0) +
-  labs(y="Cost saved per 100,000 population\n", x="\nDALYs averted per 100,000 population", color="Age group")+ 
+  expand_limits(y = 0) +
+  labs(y = "Cost saved per 100,000 population\n",
+       x = "\nDALYs averted per 100,000 population", color = "Age group") + 
   theme_bw() +
   # theme(axis.text=element_text(size=10),
   #       axis.title=element_text(size=12,face="bold"),
@@ -44,6 +44,7 @@ f5 <- ggplot(subset(datfull, scenario %in% "vax70" & reference %in% "vaxbase"),
   #       panel.grid.major = element_blank(),
   #       #panel.grid.major = element_line(colour = "grey86",size=0.5), ### these remove the minor lines
   #       panel.grid.minor = element_blank())+
-  facet_grid(v.eff~risk_f,scales="free")
+  facet_grid(v.eff~risk_f, scales = "free")
 
-ggsave('figure5.png', height = 6, width = 8, dpi=1200)
+#ggsave('figure5.png', height = 6, width = 8, dpi=1200)
+
